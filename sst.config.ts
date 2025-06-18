13e884203ac46d9dd1297ef614af9cc9bedb9081
Dan Ross-Li <me@danrossli.com>
2025-06-18 11:40:35 -0400

    add policies

--- sst.config.ts
+++ sst.config.ts
@@ -66,6 +66,8 @@ export default $config({
         viewerProtocolPolicy: "redirect-to-https",
         allowedMethods: ["GET", "HEAD"],
         cachedMethods: ["GET", "HEAD"],
+        cachePolicyId: "4135ea2d-6df8-44a3-9df3-4b5a84be39ad", // CachingDisabled
+        originRequestPolicyId: "b689b0a8-53d0-40ab-baf2-68738e2966ac",
         functionAssociations: [
           {
             eventType: "viewer-request",
