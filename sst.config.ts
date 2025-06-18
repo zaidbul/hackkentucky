a990f1fe236eab0b8f0e3ad6ad5c6b27679e4740
Dan Ross-Li <me@danrossli.com>
2025-06-18 11:42:25 -0400

    remove origin all viewers

--- sst.config.ts
+++ sst.config.ts
@@ -67,7 +67,6 @@ export default $config({
         allowedMethods: ["GET", "HEAD"],
         cachedMethods: ["GET", "HEAD"],
         cachePolicyId: "4135ea2d-6df8-44a3-9df3-4b5a84be39ad", // CachingDisabled
-        originRequestPolicyId: "b689b0a8-53d0-40ab-baf2-68738e2966ac",
         functionAssociations: [
           {
             eventType: "viewer-request",
